6fb68dbaf30ecda7d83f9eb4b186a30ee7038db1
EkamKhatri <ekamkhatri01@gmail.com>
2025-11-14 21:32:29 +0530

    Update 7.1.1.java

--- 7.1.1.java
+++ 7.1.1.java
@@ -1,8 +1,10 @@
 package exercises.java;
 public class PackageDemo {
     public void display() {
+        System.out.print("Ekam Khatri, 24csu335\n");
         System.out.println("PackageDemo executed");
     }
 }
+
 
 
