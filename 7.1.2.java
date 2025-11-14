b30d085ababeffde5bfe37b23e79e38f81f3be8f
EkamKhatri <ekamkhatri01@gmail.com>
2025-11-14 22:36:16 +0530

    Update 7.1.2.java

--- 7.1.2.java
+++ 7.1.2.java
@@ -1,10 +1,12 @@
 package exercises.java;
 public class PackageDemoDriver {
     public static void main(String[] args) {
+        System.out.print("Ekam Khatri, 24csu335\n");
         PackageDemo obj = new PackageDemo();
         obj.display();
     }
 }
+
 
 
 
