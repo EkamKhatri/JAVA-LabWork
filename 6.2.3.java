1326282a452be03bec7b67a03571d088215e642e
EkamKhatri <ekamkhatri01@gmail.com>
2025-11-14 21:20:42 +0530

    Update 6.2.3.java

--- 6.2.3.java
+++ 6.2.3.java
@@ -1,23 +1,15 @@
 public class Circle implements GeometricObject {
-    private double radius = 1.0; // Making radius private
+    protected double radius = 1.0;
 
-    // Constructor
     public Circle(double radius) {
         this.radius = radius;
     }
 
-    // Getter method for radius (optional, in case you need to access radius)
-    public double getRadius() {
-        return radius;
-    }
-
-    @Override
     public double getPerimeter() {
         return 2 * Math.PI * radius;
     }
 
-    @Override
     public double getArea() {
-        return Math.PI * Math.pow(radius, 2);
+        return Math.PI * radius * radius;
     }
 }
