86f4a9fba18d6e70d618f5a9c20a5361e75ec9cd
EkamKhatri <ekamkhatri01@gmail.com>
2025-11-14 21:23:11 +0530

    Update 6.2.4.java

--- 6.2.4.java
+++ 6.2.4.java
@@ -1,22 +1,8 @@
 public class ResizableCircle extends Circle implements Resizable {
-    
-    // Constructor
     public ResizableCircle(double radius) {
-        super(radius);  // Calls Circle constructor
+        super(radius);
     }
-
-    @Override
     public void resize(int percent) {
-        // Resizes the radius by a percentage
-        double newRadius = getRadius() * (1 + percent / 100.0);
-        // Assuming you want to update the radius after resize
-        // This sets the updated radius in the parent class
-        try {
-            java.lang.reflect.Field radiusField = Circle.class.getDeclaredField("radius");
-            radiusField.setAccessible(true); // Make private field accessible
-            radiusField.set(this, newRadius);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        radius *= (1 + percent / 100.0);
     }
 }
