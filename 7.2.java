d8c690a2113413f8eb6d8fc94e262782a0d17e14
EkamKhatri <ekamkhatri01@gmail.com>
2025-11-14 22:36:45 +0530

    Update 7.2.java

--- 7.2.java
+++ 7.2.java
@@ -1,10 +1,12 @@
 import exercises.java.PackageDemo;
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Ekam Khatri, 24csu335\n");
         PackageDemo obj = new PackageDemo();
         obj.display();
     }
 }
+
 
 
 
